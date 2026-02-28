ac3d0c5b63344b38462c997c32f6360524b3bd92
srikarthik6 <sri16karthik@gmail.com>
2026-02-28 20:53:56 +0530

    Add email sending via Flask-Mail

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 import os
 from flask import Flask, render_template, request, flash, redirect
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
+
+app.secret_key = "supersecretkey"
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
+
+mail = Mail(app)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FILE_PATH = os.path.join(BASE_DIR, "message.txt")
@@ -22,16 +34,19 @@ def contact():
         email = request.form['email']
         message = request.form['message']
 
-        with open(FILE_PATH, 'a') as f:
-            f.write(f"Name: {name}\n")
-            f.write(f"Email: {email}\n")
-            f.write(f"Message: {message}\n")
-            f.write("-" * 40 + "\n")
+        msg = Message(
+            subject=f"Portfolio Contact from {name}",
+            recipients=[os.environ.get('MAIL_USERNAME')]
+        )
+
+        msg.body = f""" Name: {name}
+                        Email: {email}
+                        Message:{message}"""
+
+        mail.send(msg)
 
         flash("Message sent successfully!")
         return redirect('/contact')
-
-    return render_template('contact.html')
 
 @app.route('/projects')
 def projects():
@@ -40,8 +55,8 @@ def projects():
          "description": "this is a basic portfolio website",
          "tech_stack": ["HTML", "TailwindCSS", "JavaScript", "Flask","Python"],
          "logo":"images/projects/portfolio_favicon.png",
-         "github":"https://github.com/sri16/portfolio",
-         "live":"https://sri16-portfolio.onrender.com/"},
+         "github":"https://github.com/srikarthikB/portfolio-flask.git",
+         "live":"https://portfolio-flask-five.vercel.app/"},
         {"name": "Interactive digital library",
          "description": "this is a digital library website",
          "tech_stack": ["Python", "Flask", "JavaScript", "TailwindCSS"],
